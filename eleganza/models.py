a1038a8d886943b014a5d96ebd9074575524306f
ip69719 <ipeters@mail.com>
2023-04-21 18:48:40 +0000

    feat: create and register booking model

--- eleganza/models.py
+++ eleganza/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Booking(models.Model):
+    """
+    Appointment Booking model
+    """
+    Customer = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
