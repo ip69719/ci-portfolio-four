d9b0f6d005c256707a05f7d989952911bd6ba1c6
ip69719 <ipeters@mail.com>
2023-04-22 00:49:05 +0000

    feat: display additional fields in admin panel and add ability to search and filter records

--- eleganza/admin.py
+++ eleganza/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Appointment
 # Register your models here.
-admin.site.register(Booking)
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('date', 'time', 'first_name', 'last_name',
+                    'phone', 'email', 'status')
+    search_fields = ['first_name', 'phone']
+    list_filter = ('date', 'status')
