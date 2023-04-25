873c7c2fff2a4aa9b72346ac911d366c56ccccad
ip69719 <ipeters@mail.com>
2023-04-25 11:18:09 +0000

    add django-allauth apps to the list of installed apps in settings.py

--- django_eleganza/settings.py
+++ django_eleganza/settings.py
@@ -40,11 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'eleganza',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
