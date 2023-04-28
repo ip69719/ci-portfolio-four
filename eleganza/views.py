048b078fb73815cc359645240a0e0fa66f4e8514
ip69719 <ipeters@mail.com>
2023-04-28 08:15:03 +0000

    feat: create view to render appointment bookings

--- eleganza/views.py
+++ eleganza/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Appointment
 
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
     return render(request, 'index.html')
+
+
+def all_appointments(request):
+    """ A view to show all appointments """
+    appointments = Appointment.objects.all()
+    context = {
+        'appointments': appointments,
+    }
+    return render(request, 'my_profile.html', context)
