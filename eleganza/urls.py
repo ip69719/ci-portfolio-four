d4a8991c46d79b8217b0e7579a5e07fc2a325bb0
ip69719 <ipeters@mail.com>
2023-04-28 08:20:44 +0000

    feat: add url path for my_profile in urls.py file

--- eleganza/urls.py
+++ eleganza/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('my_profile', views.all_appointments, name='my_profile'),
 ]
