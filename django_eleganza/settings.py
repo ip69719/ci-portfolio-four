4de53c8f00c482e888b038ac0acd936680bd2c53
ip69719 <ipeters@mail.com>
2023-04-29 00:57:32 +0000

    set debug to false

--- django_eleganza/settings.py
+++ django_eleganza/settings.py
@@ -14,12 +14,8 @@ import os
 from pathlib import Path
 
 import dj_database_url
-
 if os.path.isfile('env.py'):
     import env
-
-
-DEBUG = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = False
 
 ALLOWED_HOSTS = ['eleganza-hair-salon.herokuapp.com', 'localhost']
 
