00d97c8fc8eb6ec195ed67fdfdd09d68f6295b84
ip69719 <ipeters@mail.com>
2023-04-29 00:30:55 +0000

    fix background image

--- django_eleganza/settings.py
+++ django_eleganza/settings.py
@@ -14,8 +14,10 @@ import os
 from pathlib import Path
 
 import dj_database_url
+DEBUG = False
 if os.path.isfile('env.py'):
     import env
+    DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
 
 ALLOWED_HOSTS = ['eleganza-hair-salon.herokuapp.com', 'localhost']
 
