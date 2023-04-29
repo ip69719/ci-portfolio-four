0e55ebf54cd9de24fb4a84746877e5747af31d22
ip69719 <ipeters@mail.com>
2023-04-29 22:02:00 +0000

    feat: create the view code for appointment booking

--- eleganza/views.py
+++ eleganza/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Appointment
+from .forms import AppointmentForm
 
 
 def index(request):
@@ -8,7 +9,7 @@ def index(request):
 
 
 def all_appointments(request):
-    """ 
+    """
     A view to show all appointments.
     Returns appointment details for logged in user.
     """
@@ -19,3 +20,23 @@ def all_appointments(request):
         'appointment_form': AppointmentForm()
     }
     return render(request, 'my_profile.html', context)
+
+
+def book(request):
+    """
+    A view to display the booking form to the user.
+    Form is saved if inputs are valid and user is
+    redirected to his/her profile page.
+    """
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        appointment = form.save(commit=False)
+        if form.is_valid():
+            appointment.customer = request.user
+            appointment.save()
+            return redirect('my_profile')
+    form = AppointmentForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'book.html', context)
