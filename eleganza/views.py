118376bae14be1043710e0554e1e14df570b983c
ip69719 <ipeters@mail.com>
2023-04-29 20:52:33 +0000

    feat: add filter to all appointments view so that a user can see his/her own appointments only

--- eleganza/views.py
+++ eleganza/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 from .models import Appointment
 
 
-# Create your views here.
 def index(request):
     """ A view to return the index page """
     return render(request, 'index.html')
 
 
 def all_appointments(request):
-    """ A view to show all appointments """
-    appointments = Appointment.objects.all()
+    """ 
+    A view to show all appointments.
+    Returns appointment details for logged in user.
+    """
+    appointments = Appointment.objects.filter(customer=request.user)
+
     context = {
         'appointments': appointments,
+        'appointment_form': AppointmentForm()
     }
     return render(request, 'my_profile.html', context)
