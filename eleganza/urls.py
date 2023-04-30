4b385dc5899ee5233022ff9e242995567dd887f4
ip69719 <ipeters@mail.com>
2023-04-30 09:48:45 +0000

    feat: add url path for edit booking in urls.py file

--- eleganza/urls.py
+++ eleganza/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('my_profile', views.all_appointments, name='my_profile'),
     path('book', views.book, name='book'),
+    path('edit/<appointment_id>/', views.edit_booking, name='edit'),
 ]
