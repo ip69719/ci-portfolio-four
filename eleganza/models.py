5ee3f30241b5bab099d4246107b1a339ae7581c0
ip69719 <ipeters@mail.com>
2023-08-15 20:39:05 +0000

    feat: create drop-down with time slot for the booking form

--- eleganza/models.py
+++ eleganza/models.py
@@ -5,6 +5,22 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Confirmed"), (1, "Cancelled"))
 
 
+TIME_CHOICES = (
+    ("08:00", "08:00"),
+    ("09:00", "09:00"),
+    ("10:00", "10:00"),
+    ("11:00", "11:00"),
+    ("12:00", "12:00"),
+    ("13:00", "13:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+    ("16:00", "16:00"),
+    ("17:00", "17:00"),
+    ("18:00", "18:00"),
+    ("19:00", "19:00"),
+)
+
+
 # Create your models here.
 class Appointment(models.Model):
     """
@@ -17,5 +33,6 @@ class Appointment(models.Model):
     phone = models.CharField(max_length=15, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     date = models.DateField()
-    time = models.TimeField()
+    time = models.CharField(max_length=5, choices=TIME_CHOICES,
+                            default="08:00")
     status = models.IntegerField(choices=STATUS, default=0)
