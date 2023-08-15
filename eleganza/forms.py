874578acb12348006b4bb32410c040ed177ff559
ip69719 <ipeters@mail.com>
2023-08-15 11:28:42 +0000

    feat: create datepicker on booking form to select dates

--- eleganza/forms.py
+++ eleganza/forms.py
@@ -1,5 +1,14 @@
+import datetime
 from django import forms
 from .models import Appointment
+
+
+class DateInput(forms.DateInput):
+    """
+    Class to show datepicker,
+    code written with help from my Mentor Malia Havlicek
+    """
+    input_type = 'date'
 
 
 class AppointmentForm(forms.ModelForm):
@@ -7,3 +16,10 @@ class AppointmentForm(forms.ModelForm):
         """ inner class to define which model the form is associated with """
         model = Appointment
         fields = ['first_name', 'last_name', 'phone', 'email', 'date', 'time']
+
+        # code written with help from my Mentor Malia Havlicek
+        widgets = {
+            'date': DateInput(attrs={
+                'min': datetime.date.today()+datetime.timedelta(days=0),
+                'max': datetime.date.today()+datetime.timedelta(days=30)}),
+        }
