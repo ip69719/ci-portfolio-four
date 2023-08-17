36f4acb831005e9fe996da6e162f7f123b4dc6cc
ip69719 <ipeters@mail.com>
2023-08-17 23:58:32 +0000

    feat: add custom validation on first and last name form fields

--- eleganza/forms.py
+++ eleganza/forms.py
@@ -1,6 +1,7 @@
 import datetime
 from django import forms
 from .models import Appointment
+from django.core.exceptions import ValidationError
 
 
 class DateInput(forms.DateInput):
@@ -12,6 +13,30 @@ class DateInput(forms.DateInput):
 
 
 class AppointmentForm(forms.ModelForm):
+    """
+    Function used for multi-field validation
+    """
+    def clean(self):
+
+        # data from the form is fetched using super function
+        cleaned_data = super().clean()
+
+        # get the user submitted inputs
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
+
+        # condition to be met for the first_name field
+        if len(first_name) < 2:
+            self._errors['first_name'] = self.error_class(
+                ['First name should be at least two characters long'])
+
+        # condition to be met for the lirst_name field
+        if len(last_name) < 2:
+            self._errors['last_name'] = self.error_class(
+                ['Last name should be at least two characters long'])
+
+        return cleaned_data
+
     class Meta:
         """ inner class to define which model the form is associated with """
         model = Appointment
