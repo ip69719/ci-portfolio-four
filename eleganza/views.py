43643a6f5a2e466faa3b05b95fd5b2b50c3344d7
ip69719 <ipeters@mail.com>
2023-08-19 18:53:58 +0000

    DOCS: trigger error 500

--- eleganza/views.py
+++ eleganza/views.py
@@ -44,7 +44,8 @@ def book(request):
                 request, f'Thank you! We look forward to seeing you.')
             return redirect('my_profile')
         else:
-            return render(request, 'book.html', {'form': form})
+            # return render(request, 'book.html', {'form': form})
+            return render(request, 'error_500.html')
     else:
         form = AppointmentForm(None)
         context = {
